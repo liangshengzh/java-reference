77854d77f1dfcdff2adb28f97ca8f60ac5969c08
Jimmy Zhong <liangsheng.zhong@outlook.com>
2014-08-04 15:19:42 +0800

    change the project structure

---
+++ hibernate/src/main/java/info/liangsheng/hibernate/Main.java
@@ -0,0 +1,11 @@
+package info.liangsheng.hibernate;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhongl
+ * Date: 8/4/14
+ * Time: 3:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
