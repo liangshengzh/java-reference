4ea65480309072e9e185d30563ae8e43d0fd2994
liangsheng <liangsheng.zhong@outlook.com>
2014-09-11 21:48:04 +0800

    add string reverse

---
+++ java-core/src/main/java/info/liangsheng/StringReverse.java
@@ -0,0 +1,25 @@
+package info.liangsheng;
+
+/**
+ * Created by liangsheng on 9/11/14.
+ */
+public class StringReverse {
+    public static void main(String[] args) {
+        String str = "jimmy";
+
+        StringBuffer sb = new StringBuffer(str);
+        System.out.println(sb.reverse());
+
+        System.out.println(reverse(str));
+
+
+    }
+
+    public static String reverse(String str){
+        StringBuffer sb = new StringBuffer();
+        for(int i =str.length()-1;i>=0;i--){
+            sb.append(str.charAt(i));
+        }
+       return sb.toString();
+    }
+}
